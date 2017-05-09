36c9fb0f9b318eee9bb33107ed99382279d50770
nitanka <nitankagogoi@gmail.com>
2017-05-09 11:30:40 +0530

    Restructuring of the directory

--- spark-master/start-master.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "--------------------STARTING SPARK MASTER-------------------------"
-
-spark-class org.apache.spark.deploy.master.Master --host 0.0.0.0
-
--- spark-slave/start-slave.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "--------------------STARTING SPARK MASTER-------------------------"
-
-spark-class org.apache.spark.deploy.worker.Worker spark://spark-master:7077
-

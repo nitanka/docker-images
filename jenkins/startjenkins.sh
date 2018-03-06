4de954987c999f1ddf75960e55e5cc90efe52f72
Ubuntu <ubuntu@ip-172-31-15-96.us-east-2.compute.internal>
2018-03-06 10:42:14 +0000

    added helper script for running jenkins

---
+++ jenkins/startjenkins.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+PATH=/bin:/usr/bin:/sbin:/usr/sbin
+
+NAME=jenkins
+
+[ -r /etc/default/$NAME ] && . /etc/default/$NAME
+
+
+# load environments
+if [ -r /etc/default/locale ]; then
+  . /etc/default/locale
+  export LANG LANGUAGE
+elif [ -r /etc/environment ]; then
+  . /etc/environment
+  export LANG LANGUAGE
+fi
+
+# Load the VERBOSE setting and other rcS variables
+. /lib/init/vars.sh
+
+# Define LSB log_* functions.
+# Depend on lsb-base (>= 3.0-6) to ensure that this file is present.
+. /lib/lsb/init-functions
+
+do_start()
+{
+    $DAEMON $DAEMON_ARGS -- $JAVA $JAVA_ARGS -jar $JENKINS_WAR $JENKINS_ARG
+}
